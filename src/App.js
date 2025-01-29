eef85092289209b55c006a0d6c5040e034a432e8
ganeshvrma <135155713+ganeshvrma@users.noreply.github.com>
2025-01-29 19:46:32 +0530

    feat/twcss add

--- src/App.js
+++ src/App.js
@@ -3,32 +3,32 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const decreHandler = () => {
+  const decrementHandler = () => {
     setCount(count - 1);
   };
-  const increHandler = () => {
+  const incrementHandler = () => {
     setCount(count + 1);
   };
   const resetHandler = () => {
     setCount(0);
   };
   return (
-    <div className=" w-[100vw] h-[100vh] flex  justify-center items-center flex-col gap-10">
-      <div>
-        <h1 className="font-semibold  "> Increment & Decrement</h1>
+    <div className=" w-[100vw] h-[100vh]  bg-blue-200 flex  justify-center items-center flex-col gap-10">
+      <div
+         className="font-semibold text-2xl "> Increment & Decrement
         
       </div>
-      <div className="flex justify-center gap-12 ">
-        <button className=" border-l-2 text-center w-20 text-5xl" onClick={decreHandler}>
+      <div className="flex  rounded-sm justify-center gap-12 py-4 text-[25px] bg-white ">
+        <button className=" border-r-2 text-center border-[#bfbfbf] w-20 text-5xl" onClick={decrementHandler}>
           -
         </button>
-        <div className="font-bold gap-12 text-5xl">{count}</div>
-        <button className="border-l-2 text-center w-20 text-5xl" onClick={increHandler}>
+        <div className="font-semibold gap-12 text-5xl">{count}</div>
+        <button className="border-l-2 border-[#bfbfbf] text-center w-20 text-5xl" onClick={incrementHandler}>
           +
         </button>
       </div>
       <div>
-        <button className="bg-yellow-100 px-4 py-3" onClick={resetHandler}>
+        <button className="bg-sky-100  px-4 py-3 rounded-full" onClick={resetHandler}>
           Reset
         </button>
       </div>
